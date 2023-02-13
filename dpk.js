b522a021529c793c3460203570dbab1a365e145d
ronwthompson <moliviank@hotmail.com>
2023-02-13 15:22:30 -0800

    refactored dpk with notes

--- dpk.js
+++ dpk.js
@@ -3,26 +3,36 @@ const crypto = require("crypto");
 exports.deterministicPartitionKey = (event) => {
   const TRIVIAL_PARTITION_KEY = "0";
   const MAX_PARTITION_KEY_LENGTH = 256;
+
+  if (!event) return TRIVIAL_PARTITION_KEY
+
+  if (!event.partitionKey) {
+    return crypto.createHash("sha3-512").update(JSON.stringify(event)).digest("hex");
+  }
+
   let candidate;
 
-  if (event) {
-    if (event.partitionKey) {
-      candidate = event.partitionKey;
-    } else {
-      const data = JSON.stringify(event);
-      candidate = crypto.createHash("sha3-512").update(data).digest("hex");
-    }
+  if (typeof event.partitionKey !== "string") {
+    candidate = JSON.stringify(event.partitionKey);
+  } else {
+    candidate = event.partitionKey;
   }
 
-  if (candidate) {
-    if (typeof candidate !== "string") {
-      candidate = JSON.stringify(candidate);
-    }
-  } else {
-    candidate = TRIVIAL_PARTITION_KEY;
-  }
   if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-    candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
+    return crypto.createHash("sha3-512").update(candidate).digest("hex");
   }
-  return candidate;
+
+  return candidate
 };
+
+// Refactoring Notes:
+
+// The nested if statements are difficult to follow from beginning to end. We can 
+// reduce the number of branching paths by isolating a few of them in the beginning. 
+// For example, we don't need to go through the whole flow chart if no event is 
+// found. Additionally, we just return hash from the event if no partitionKey exists.
+// In the rest of the function, we make simple checks to update the candidate
+// variable to ensure it meets our requirements (is a string, is within the max 
+// length) before finishing. I considered using a switch statement instead but we
+// want to make decisions on different properties of our event, which a switch does
+// not support.
--- dpk.test.js
+++ dpk.test.js
@@ -7,6 +7,14 @@ describe("deterministicPartitionKey", () => {
     expect(trivialKey).toBe("0");
   });
 
+  it("Returns the a key based on the event if no partitionKey property is found", () => {
+    const invalidTestEvent = {
+      invalidProperty: "Hello world!"
+    }
+    const trivialKey = deterministicPartitionKey(invalidTestEvent);
+    expect(trivialKey).toBe(crypto.createHash("sha3-512").update(JSON.stringify(invalidTestEvent)).digest("hex"));
+  });
+
   it("Returns the partitionKey if valid", () => {
     const validTestEvent = {
       partitionKey: crypto.createHash("sha3-512").digest("hex")
@@ -15,20 +23,22 @@ describe("deterministicPartitionKey", () => {
     expect(trivialKey).toBe(validTestEvent.partitionKey);
   });
 
-  it("Returns the partitionKey as a string when given a number", () => {
+  it("Returns the partitionKey as a string when given a number but otherwise unchanged", () => {
     const validTestEvent = {
       partitionKey: 169173111231915
     }
     const trivialKey = deterministicPartitionKey(validTestEvent);
     expect(typeof trivialKey).toBe("string");
+    expect(trivialKey).toBe(JSON.stringify(validTestEvent.partitionKey));
   });
 
-  it("Returns the partitionKey as a string when given an object", () => {
+  it("Returns the partitionKey as a string when given an object but otherwise unchanged", () => {
     const validTestEvent = {
       partitionKey: { text: "Hello world!"}
     }
     const trivialKey = deterministicPartitionKey(validTestEvent);
     expect(typeof trivialKey).toBe("string");
+    expect(trivialKey).toBe(JSON.stringify(validTestEvent.partitionKey));
   });
 
   it("Returns the updated partitionKey when given key too long", () => {
