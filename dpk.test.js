d57881a4af8a6924f963c69b309f2a433acc6c47
ronwthompson <moliviank@hotmail.com>
2023-02-13 14:55:33 -0800

    add refactor tests

--- dpk.test.js
+++ dpk.test.js
@@ -1,8 +1,41 @@
 const { deterministicPartitionKey } = require("./dpk");
+const crypto = require("crypto");
 
 describe("deterministicPartitionKey", () => {
   it("Returns the literal '0' when given no input", () => {
     const trivialKey = deterministicPartitionKey();
     expect(trivialKey).toBe("0");
+  });
+
+  it("Returns the partitionKey if valid", () => {
+    const validTestEvent = {
+      partitionKey: crypto.createHash("sha3-512").digest("hex")
+    }
+    const trivialKey = deterministicPartitionKey(validTestEvent);
+    expect(trivialKey).toBe(validTestEvent.partitionKey);
+  });
+
+  it("Returns the partitionKey as a string when given a number", () => {
+    const validTestEvent = {
+      partitionKey: 169173111231915
+    }
+    const trivialKey = deterministicPartitionKey(validTestEvent);
+    expect(typeof trivialKey).toBe("string");
+  });
+
+  it("Returns the partitionKey as a string when given an object", () => {
+    const validTestEvent = {
+      partitionKey: { text: "Hello world!"}
+    }
+    const trivialKey = deterministicPartitionKey(validTestEvent);
+    expect(typeof trivialKey).toBe("string");
+  });
+
+  it("Returns the updated partitionKey when given key too long", () => {
+    const invalidTestEvent = {
+      partitionKey: "a69f73cca23a9ac5c8b567dc185a756e97c982164fe25859e0d1dcc1475c80a615b2123af1f5f94c11e3e9402c3ac558f500199d95b6d3e301758586281dcd26a69f73cca23a9ac5c8b567dc185a756e97c982164fe25859e0d1dcc1475c80a615b2123af1f5f94c11e3e9402c3ac558f500199d95b6d3e301758586281dcd26a69f73cca23a9ac5c8b567dc185a756e97c982164fe25859e0d1dcc1475c80a615b2123af1f5f94c11e3e9402c3ac558f500199d95b6d3e301758586281dcd26a69f73cca23a9ac5c8b567dc185a756e97c982164fe25859e0d1dcc1475c80a615b2123af1f5f94c11e3e9402c3ac558f500199d95b6d3e301758586281dcd26"
+    }
+    const trivialKey = deterministicPartitionKey(invalidTestEvent);
+    expect(trivialKey.length).toBe(128);
   });
 });
